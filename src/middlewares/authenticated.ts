bc77c3f3ea47e899f0633dc963f92f26ef8f728e
costamarcelow <costamarcelo94@gmail.com>
2021-11-17 16:24:24 -0300

    Edição package

--- src/middlewares/authenticated.ts
+++ src/middlewares/authenticated.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { verify } from 'jsonwebtoken';
 import authConfig from '../config/auth';
-import APIError from "../shared/APIError";
+//import APIError from "../shared/APIError";
 
 
 interface Token {
